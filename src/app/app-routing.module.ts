b2fc433b195f235fa6b06304e53374c96d8e94af
Anand Mohan <im.anand.m@gmail.com>
2021-09-28 08:40:04 -0700

    added nav link for sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes, RouterModule} from '@angular/router';
 //import {ProductFormComponent} from "./product-form/product-form.component";
 import {ProductListComponent} from "./product-list/product-list.component";
 import {ProductGridComponent} from "./product-grid/product-grid.component";
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 const routes: Routes = [
   {path: 'products', component: ProductListComponent},
-  {path: 'main', component: ProductGridComponent}
+  {path: 'main', component: ProductGridComponent},
+  {path: 'signup', component: UserFormComponent} 
 ];
 
 @NgModule({
